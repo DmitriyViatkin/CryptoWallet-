5f447cbbc4d350d1b3a95931efdfc1303df6e5ee
Dmitriy <ViatkinDima@gmail.com>
2026-06-16 21:17:12 +0300

    Implemment wallet routers +to do

--- apps/rest_api_service/src/users/routers/wallets/all_wallet_user.py
+++ apps/rest_api_service/src/users/routers/wallets/all_wallet_user.py
@@ -21,7 +21,7 @@ async def get_transaction(
     if not wallets:
         raise HTTPException(status_code=404, detail="Wallet not found")
 
-
+# TODO Добавить получение баланса из редиса пр  выводе кошельков
 
 
     return  AllWalletsResponse(items=wallets)
